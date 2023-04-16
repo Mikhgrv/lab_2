8a944038bb8446a7652073155df6ca1dd43e3999
Michailo <michgrv@gmail.com>
2023-04-16 22:05:27 +0300

    i hoope the last update

---
+++ 2_task/php_get.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Привіт <?php echo $_GET["name"]; ?> <?php echo $_GET["surname"]; ?><br>
+Ти володієш смартфоном <?php echo $_GET["com"]; ?><br>
+Рік випуску якого <?php echo $_GET["year"]; ?>
+
+</body>
+</html>
